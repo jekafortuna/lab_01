ddd9ae781978df76d8b39fffed9e092caaf5fcd5
jekafortuna <evgeniy.fortuna@gmail.com>
2016-12-18 23:41:02 +0200

    Traffic Journal

---
+++ src/main/java/TrafficJournal/Transport.java
@@ -0,0 +1,36 @@
+package TrafficJournal;
+
+/**
+ * Created on 15.12.2016.
+ *
+ * super class to all vehicles
+ */
+
+public abstract class Transport {
+
+    /**
+     * parameters of vehicles
+     * @return
+     */
+    public abstract String model();
+    public abstract String number();
+
+    @Override
+    public String toString(){
+        return "Model of vehicle is : " + model() + ", and the number is: " + number() + '\'';
+    }
+
+    @Override
+    public boolean equals(Object obj){
+
+        if (this == obj){
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+
+        Transport temp = (Transport) obj;
+        return (temp.number() == this.number()) && (temp.model() == this.model());
+    }
+}
